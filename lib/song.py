13e527c25e7fd6317c35d5dca1c65ee597570b91
Shakira Syevuo <shakirasyevuo@gmail.com>
2025-02-18 19:20:28 +0300

    Completed lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,45 @@
 class Song:
-    pass
+    # class attributes for the class Song
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {} # class attribute to store the count of genres
+    artist_count = {} # class attribute to store the artist count
+
+    # class methods for the class Song
+    def __init__(self, name, artist, genre):
+        
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.add_song_to_count()
+        Song.add_to_genres(self.genre)
+        Song.add_to_artists(self.artist)
+        Song.update_genre_count(self.genre)
+        Song.update_artist_count(self.artist)
+
+    @classmethod
+    def add_song_to_count(cls, increment = 1):
+        cls.count += increment
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def update_genre_count(cls, genre):
+        cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
+
+    @classmethod
+    def update_artist_count(cls, artist):
+        cls.artist_count[artist] = cls.artist_count.get(artist, 0) + 1
+
+
+
